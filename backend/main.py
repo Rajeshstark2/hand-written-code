b7448fe979c92d8381c3855206b21422514b4988
rajesh k <143821418+Rajeshstark2@users.noreply.github.com>
2025-03-24 14:30:46 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hand-written-code-2.onrender.com"],  # React app URL
+    allow_origins=["https://handcode.onrender.com"],  # React app URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
